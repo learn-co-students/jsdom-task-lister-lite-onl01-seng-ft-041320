3c542d6ea1d0d9a299baba0b8062eca75cfa4158
raspimeteo <jjczon@gmail.com>
2020-07-15 07:47:58 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+	// your code here
+	document.addEventListener('submit', event => {
+		// prevent page refresh on form submission
+		event.preventDefault();
+		let input = document.getElementById("new-task-description").value;
+		let myList = document.getElementsByTagName('ul')[0]
+		const listItem = document.createElement("li");
+		const listValue = document.createTextNode(input);
+		listItem.appendChild(listValue);
+		myList.appendChild(listItem);
+		event.target.reset()
+	});
 });
