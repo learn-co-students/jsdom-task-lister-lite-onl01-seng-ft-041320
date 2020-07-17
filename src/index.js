73eb27be9d670af06bc33331f2887e4e06c0437e
osha7 <ogroetz@gmail.com>
2020-07-17 17:59:29 -0600

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const taskForm = document.getElementById("create-task-form");
+  const taskDescription = document.getElementById("new-task-description");
+
+  
+  // where will new tasks live:
+  const tasks = document.getElementById("tasks");
+
+  //event listener (for adding list items):
+
+  function createNewTask(e) {
+    e.preventDefault()
+    let input = taskDescription;
+    tasks.innerHTML += `<li>${input.value} <button id="remove-button" data-description="${input.value}">X</button></li>`;
+    event.target.reset()
+  }
+
+  taskForm.addEventListener('submit', createNewTask);
+  
+   //event listener (for deleting list items):
+  function removeTaskButton(e) {
+    if ( e.target.tagName == "BUTTON") {
+      e.target.parentElement.remove();
+    }
+  }
+  tasks.addEventListener('click', removeTaskButton);
+  
 });
+
+
+
+
+
