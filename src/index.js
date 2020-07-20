9e8bcf0b1c4f362d22817458899c2630150d2378
Stephen McBride <stevemcbride3@gmail.com>
2020-07-20 19:56:07 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
 });
+document.addEventListener("submit", () => {
+  addTask(document.getElementById("new-task-description").value);
+  event.preventDefault();
+});
+
+function addTask(description){
+  let tasks = document.getElementById("tasks");
+  let newTask = document.createElement("li")
+  newTask.innerText = description;
+  tasks.append(newTask);
+};
