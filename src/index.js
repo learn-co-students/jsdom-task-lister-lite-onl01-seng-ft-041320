84214bd63828cacc39b80c80638acd88d5849afe
github username <github email address>
2020-07-22 10:47:14 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+document.addEventListener("DOMContentLoaded", () => { 
+  const nodeList = document.querySelectorAll('input');
+
+  const submitButton = nodeList[1];
+
+  const addTodoArea = document.querySelector('form#create-task-form');
+
+  const todoList = document.querySelector('div#list');
+
+  const tasksList = todoList.querySelector('ul#tasks');
+
+  const taskDescription = addTodoArea.querySelector('input[type=text]');
+
+  /*const deleteButton = document.querySelector("button.remove-button")*/
+
+function createNewTask(e) {
+  e.preventDefault()
+  let input = taskDescription;
+  tasksList.innerHTML += `<li>${input.value} <button class="remove-button" data-destcription="${input.value}">X</button></li>`;
+  taskDescription.value = ""
+};
+
+function deleteTask(e) {
+  if ( e.target.tagName == "BUTTON") {
+  e.target.parentElement.remove();
+  }
+}
+
+  submitButton.addEventListener("click", createNewTask);
+  todoList.addEventListener("click", deleteTask);
+
 });
