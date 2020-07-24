b74f1cb5cbf08a88c6a2dcc9e98dcd653ad9be75
Dhrubot <dhrubo.dh@gmail.com>
2020-07-24 06:14:42 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
+const form = document.getElementById('create-task-form');
+const description = document.getElementById('new-task-description');
+const tasksList = document.getElementById('tasks');
+
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+
+  form.addEventListener("submit", (e) => {
+    addNewTask();
+    e.preventDefault();
+  });
+
+  document.addEventListener('click', (e) => {
+    removeTask(e);
+  });
+
 });
+
+function addNewTask() {
+  let task = description.value;
+  let newTaskLi = document.createElement('li');
+
+  newTaskLi.innerHTML = `${task} <button>&#88;</button>`;;
+
+  tasksList.appendChild(newTaskLi);
+};
+
+function removeTask(event) {
+  if (event.target.tagName == "BUTTON") {
+    event.target.parentElement.remove();
+  }
+}
