8c4216993a5216a0c8207108c7f031ae5cc16dfa
Franklyn Rodriguez <franklynr92@gmail.com>
2020-07-31 22:14:42 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
+const taskDesc = document.getElementById("new-task-description").value
+const submitTask = document.get
+
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
 });
+
+
+// As a user, I should be able to type a task into the input field. [X]
+//As a user, I should be able to click some form of a submit button. []
+//As a user, the task string that I provided should appear on the DOM after the submit button has been activated. []
